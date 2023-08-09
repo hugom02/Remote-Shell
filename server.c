ca6474f11aabd8d86b6543120e4dd5d026fc7a92
hugom02 <80351096+hugom02@users.noreply.github.com>
2023-08-09 16:01:50 +0200

    Update server.c

--- server.c
+++ server.c
@@ -13,4 +13,26 @@ int bindSocket(int socketFD, char* port, char* ip) {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
+    // Renvoie une liste de tous les résultats
+    if (ip[0] == 0) { // '\0' == 0
+        rv = getaddrinfo(NULL, port, &hints, &res);
+    } else {
+        rv = getaddrinfo(ip, port, &hints, &res);
+    }
+    if (rv != 0) {
+        close(socketFD);
+        printf("getaddrinfo: %s\n", gai_strerror(rv));
+        return -1;
+    }
+    // Lier à la première adresse possible
+    for (binding = res; res != NULL; res = res->ai_next) {
+        if (bind(socketFD, binding->ai_addr, binding->ai_addrlen) == -1) {
+            continue;
+        } else {
+            freeaddrinfo(res); // Le liant pointe vers res, les deux sont donc libérés
+            return 0;
+        }
 
+    }
+    return -1;
+}
