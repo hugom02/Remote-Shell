504cb02b85324424e4e9b789b4c3afb4a9c75cff
hugom02 <80351096+hugom02@users.noreply.github.com>
2023-08-09 15:56:41 +0200

    Update server.c

--- server.c
+++ server.c
@@ -3,3 +3,14 @@
 int createSocket() {
     return socket(AF_INET, SOCK_STREAM, 0);
 }
+
+int bindSocket(int socketFD, char* port, char* ip) {
+    // Création d'un socket
+    int rv;
+    struct addrinfo hints, *res, *binding;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+
