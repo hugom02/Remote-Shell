f61a212c8e38b27f6450dcca3c120a1bde7e60b7
hugom02 <80351096+hugom02@users.noreply.github.com>
2023-08-10 18:01:54 +0200

    Update client.c

--- src/client.c
+++ src/client.c
@@ -10,3 +10,19 @@ int connectToServer(char* port, char* ip) {
 	// Le serveur est IPv4 et le type de socket est STREAM
     	hints.ai_family = AF_INET; // INET = IPv4
     	hints.ai_socktype = SOCK_STREAM;
+
+	// Renvoie une liste de tous les résultats
+	if (ip[0] == 0) {
+		rv = getaddrinfo(NULL, port, &hints, &res);
+	} else {
+		rv = getaddrinfo(ip, port, &hints, &res);
+	}
+	
+	if (rv != 0) {
+		return -1;
+	}
+
+	// Pas de résultat
+	if (res == NULL) {
+		return -2;
+	}
