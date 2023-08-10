4fc1d76f1b7ba74b45b5ae8af9946ec2d2cdd9d1
hugom02 <80351096+hugom02@users.noreply.github.com>
2023-08-10 17:30:48 +0200

    Update client.c

--- src/client.c
+++ src/client.c
@@ -1,1 +1,12 @@
+#include "../headers/client.h"
 
+int connectToServer(char* port, char* ip) {
+	// Récupérer l'adresse du serveur
+	struct addrinfo hints, *res, *attempt;
+	// Initialiser tous les indices à 0
+	memset(&hints, 0, sizeof(struct addrinfo));
+	int rv;
+	int serverSocket;
+	// Le serveur est IPv4 et le type de socket est STREAM
+    hints.ai_family = AF_INET; // INET = IPv4
+    hints.ai_socktype = SOCK_STREAM;
